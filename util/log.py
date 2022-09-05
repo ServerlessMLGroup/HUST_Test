d46e486528ea6b8c12f9597e4ef7eedb2739902d
邓俊骁 <dengjunxiao.hust@bytedance.com>
2022-09-05 21:02:54 +0800

    log

--- util/log.py
+++ util/log.py
@@ -2,11 +2,13 @@ import datetime
 import logging
 import os
 
+import colorlog as colorlog
+console_formatter = colorlog.ColoredFormatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-def CtxInfo(func_file_name, info):
-    current_time = datetime.datetime.now()
-    log_file_path = os.getcwd()
-    log_file_path += '/logs'
+file_formatter = colorlog.ColoredFormatter(fmt='[%(asctime)s.%(msecs)03d] %(filename)s -> line:%(lineno)d [%(levelname)s] : %(message)s')
+
+
+def get_logger():
     # 创建日志器对象
     logger = logging.getLogger()
 
@@ -15,30 +17,75 @@ def CtxInfo(func_file_name, info):
 
     # 添加控制台handler，用于输出日志到控制台
     console_handler = logging.StreamHandler()
-    # 添加日志文件handler，用于输出日志到文件中
+
+    # 将handler添加到日志器中
+    logger.addHandler(console_handler)
+
+    # 设置格式并赋予handler
+    console_handler.setFormatter(console_formatter)
+    return logger
+
+
+def init_file(func_file_name):
+    current_time = datetime.datetime.now()
+    dir_name, _ = os.path.split(os.path.abspath(__file__))
+    log_file_path = os.path.dirname(dir_name) + '/logs'
     log_file_name = ('%s_%s_%s_%s.txt' %
                      (func_file_name, current_time.month, current_time.day, current_time.hour))
     if not os.path.exists(log_file_path):
         os.mkdir(log_file_path)
-    os.path.join(log_file_path, log_file_name)
-    f = open(log_file_path + "/" + log_file_name)
+
+    file_full_name = log_file_path + "/" + log_file_name
+    f = open(file_full_name, "a+")
     f.close()
-    file_handler = logging.FileHandler(filename=log_file_name, encoding='UTF-8')
+    return file_full_name
 
-    # 将handler添加到日志器中
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
 
-    # 设置格式并赋予handler
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    console_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
+class Log:
 
-    # 输出不同级别日志
-    logger.debug("============【开始测试】====================")
-    logger.info("============【开始测试】====================")
-    logger.warning("============【开始测试】====================")
-    logger.critical("============【开始测试】====================")
-    logger.error("============【开始测试】====================")
+    logger = get_logger()
 
-    logger.info(info)
+    @staticmethod
+    def debug(func_file_name, msg):
+        file_full_name = init_file(func_file_name)
+        file_handler = logging.FileHandler(filename=file_full_name, encoding='UTF-8')
+        Log.logger.addHandler(file_handler)
+        file_handler.setFormatter(file_formatter)
+        Log.logger.debug(("\033[1;32m {} \033[0m").format(msg))
+        Log.logger.removeHandler(file_handler)
+
+    @staticmethod
+    def info(func_file_name, msg):
+        file_full_name = init_file(func_file_name)
+        file_handler = logging.FileHandler(filename=file_full_name, encoding='UTF-8')
+        Log.logger.addHandler(file_handler)
+        file_handler.setFormatter(file_formatter)
+        Log.logger.info(("\033[1;29m {} \033[0m").format(msg))
+        Log.logger.removeHandler(file_handler)
+
+    @staticmethod
+    def warning(func_file_name, msg):
+        file_full_name = init_file(func_file_name)
+        file_handler = logging.FileHandler(filename=file_full_name, encoding='UTF-8')
+        Log.logger.addHandler(file_handler)
+        file_handler.setFormatter(file_formatter)
+        Log.logger.warning(("\033[1;33m {} \033[0m").format(msg))
+        Log.logger.removeHandler(file_handler)
+
+    @staticmethod
+    def error(func_file_name, msg):
+        file_full_name = init_file(func_file_name)
+        file_handler = logging.FileHandler(filename=file_full_name, encoding='UTF-8')
+        Log.logger.addHandler(file_handler)
+        file_handler.setFormatter(file_formatter)
+        Log.logger.error(("\033[1;31m {} \033[0m").format(msg))
+        Log.logger.removeHandler(file_handler)
+
+    @staticmethod
+    def critical(func_file_name, msg):
+        file_full_name = init_file(func_file_name)
+        file_handler = logging.FileHandler(filename=file_full_name, encoding='UTF-8')
+        Log.logger.addHandler(file_handler)
+        file_handler.setFormatter(file_formatter)
+        Log.logger.critical(("\033[1;36m {} \033[0m").format(msg))
+        Log.logger.removeHandler(file_handler)
