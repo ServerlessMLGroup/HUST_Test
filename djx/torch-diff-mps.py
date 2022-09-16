bf192b5124f7b11ccbd79c5ad5426f4a6aa2e623
husterdjx <76643896@qq.com>
2022-09-16 11:21:34 +0800

    change batch_size

--- djx/torch-diff-mps.py
+++ djx/torch-diff-mps.py
@@ -107,17 +107,17 @@ def main():
     worker_list = []
     worker_meg_list = []
 
-    for i in range(1, 3):
+    for i in range(1, 2):
         p_parent_worker, p_child_worker = mp.Pipe()
         os.environ['CUDA_MPS_ACTIVE_THREAD_PERCENTAGE'] = "10"
         worker = WorkerProc(("worker-10-%d" % i), p_child_worker, mps_percentage=10, batch_size=8, nruns=300)
         worker.start()
         worker_meg_list.append(p_parent_worker)
         worker_list.append(worker)
-    for i in range(1, 5):
+    for i in range(1, 4):
         p_parent_worker, p_child_worker = mp.Pipe()
-        os.environ['CUDA_MPS_ACTIVE_THREAD_PERCENTAGE'] = "20"
-        worker = WorkerProc(("worker-20-%d" % i), p_child_worker, mps_percentage=20, batch_size=16, nruns=300)
+        os.environ['CUDA_MPS_ACTIVE_THREAD_PERCENTAGE'] = "30"
+        worker = WorkerProc(("worker-20-%d" % i), p_child_worker, mps_percentage=30, batch_size=32, nruns=300)
         worker.start()
         worker_meg_list.append(p_parent_worker)
         worker_list.append(worker)
