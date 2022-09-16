68dc375ad6b159cfd29e1efd907825184c4e60d2
husterdjx <76643896@qq.com>
2022-09-16 13:42:37 +0800

    change mps to 32-32-32-10

--- djx/torch-diff-mps.py
+++ djx/torch-diff-mps.py
@@ -107,21 +107,21 @@ def main():
     worker_list = []
     worker_meg_list = []
 
-    for i in range(1, 3):
+    for i in range(1, 4):
         p_parent_worker, p_child_worker = mp.Pipe()
-        os.environ['CUDA_MPS_ACTIVE_THREAD_PERCENTAGE'] = "60"
-        worker = WorkerProc(("worker-60-%d" % i), p_child_worker, mps_percentage=60, batch_size=64, nruns=300)
+        os.environ['CUDA_MPS_ACTIVE_THREAD_PERCENTAGE'] = "32"
+        worker = WorkerProc(("worker-32-%d" % i), p_child_worker, mps_percentage=32, batch_size=32, nruns=300)
         worker.start()
         worker_meg_list.append(p_parent_worker)
         worker_list.append(worker)
 
-    # for i in range(1, 2):
-    #     p_parent_worker, p_child_worker = mp.Pipe()
-    #     os.environ['CUDA_MPS_ACTIVE_THREAD_PERCENTAGE'] = "20"
-    #     worker = WorkerProc(("worker-20-%d" % i), p_child_worker, mps_percentage=20, batch_size=16, nruns=300)
-    #     worker.start()
-    #     worker_meg_list.append(p_parent_worker)
-    #     worker_list.append(worker)
+    for i in range(1, 2):
+        p_parent_worker, p_child_worker = mp.Pipe()
+        os.environ['CUDA_MPS_ACTIVE_THREAD_PERCENTAGE'] = "10"
+        worker = WorkerProc(("worker-10-%d" % i), p_child_worker, mps_percentage=10, batch_size=8, nruns=300)
+        worker.start()
+        worker_meg_list.append(p_parent_worker)
+        worker_list.append(worker)
 
     for worker_channel in worker_meg_list:
         worker_channel.send('BEGIN')
