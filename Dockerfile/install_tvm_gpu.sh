2b9d36ee512c95936344dcab5e03f896a07b4682
husterdjx <76643896@qq.com>
2022-09-25 19:55:33 +0800

    change Dockerfile to mytvm:v2

--- Dockerfile/install_tvm_gpu.sh
+++ Dockerfile/install_tvm_gpu.sh
@@ -19,15 +19,15 @@
 sudo apt-get update
 sudo apt-get install -y python3 python3-dev python3-setuptools gcc libtinfo-dev zlib1g-dev build-essential cmake libedit-dev libxml2-dev
 
-cd /
+cd /usr
 git clone https://github.com/apache/tvm tvm --recursive
-cd /tvm
+cd /usr/tvm
 
-echo set\(USE_LLVM llvm-config-8\) >> config.cmake
+echo set\(USE_LLVM ON\) >> config.cmake
 echo set\(USE_CUDA ON\) >> config.cmake
 echo set\(USE_CUDNN ON\) >> config.cmake
 echo set\(USE_BLAS openblas\) >> config.cmake
-echo set\(USE_CUBLAS ON\)
+echo set\(USE_CUBLAS ON\) >> config.cmake
 mkdir build
 cp cmake/config.cmake build
 
