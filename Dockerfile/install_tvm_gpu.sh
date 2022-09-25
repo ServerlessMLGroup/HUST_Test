b29f45e57b56bf685031e1fd36f52aad7629d6e1
husterdjx <76643896@qq.com>
2022-09-25 17:29:54 +0800

    delete USE_LLVM in install_tvm_gpu

--- Dockerfile/install_tvm_gpu.sh
+++ Dockerfile/install_tvm_gpu.sh
@@ -26,7 +26,7 @@ cd /usr/tvm
 # checkout a hash-tag
 git checkout 4b13bf668edc7099b38d463e5db94ebc96c80470
 
-echo set\(USE_LLVM llvm-config-8\) >> config.cmake
+# echo set\(USE_LLVM llvm-config-8\) >> config.cmake
 echo set\(USE_CUDA ON\) >> config.cmake
 echo set\(USE_CUDNN ON\) >> config.cmake
 echo set\(USE_BLAS openblas\) >> config.cmake
