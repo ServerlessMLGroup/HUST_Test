93c6f3d701d15980151edf222173c1af0f15e8cb
husterdjx <76643896@qq.com>
2022-09-25 16:47:30 +0800

    python api tvm : generate cuda code

--- djx/tvm-cu-compile.py
+++ djx/tvm-cu-compile.py
@@ -1,4 +1,10 @@
-from tvm.driver import tvmc
+import onnx
+from tvm.contrib.download import download_testdata
+from PIL import Image
+import numpy as np
+import tvm.relay as relay
+import tvm
+from tvm.contrib import graph_executor
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -16,6 +22,51 @@ import os
 os.environ['CUDA_VISIBLE_DEVICES'] = ("%d" % args.gpu_no)
 
 if __name__ == '__main__':
-    model = tvmc.load("/model/resnet50-v2-7.onnx") #Step 1: Load
-    tvmc.tune(model, target="cuda")
-    package = tvmc.compile(model, target="cuda", package_path="/cu")
+    model_url = (
+        "https://github.com/onnx/models/raw/main/"
+        "vision/classification/resnet/model/"
+        "resnet50-v2-7.onnx"
+    )
+
+    model_path = download_testdata(model_url, "resnet50-v2-7.onnx", module="onnx")
+    onnx_model = onnx.load(model_path)
+
+    img_url = "https://s3.amazonaws.com/model-server/inputs/kitten.jpg"
+    img_path = download_testdata(img_url, "imagenet_cat.png", module="data")
+
+    # Resize it to 224x224
+    resized_image = Image.open(img_path).resize((224, 224))
+    img_data = np.asarray(resized_image).astype("float32")
+
+    # Our input image is in HWC layout while ONNX expects CHW input, so convert the array
+    img_data = np.transpose(img_data, (2, 0, 1))
+
+    # Normalize according to the ImageNet input specification
+    imagenet_mean = np.array([0.485, 0.456, 0.406]).reshape((3, 1, 1))
+    imagenet_stddev = np.array([0.229, 0.224, 0.225]).reshape((3, 1, 1))
+    norm_img_data = (img_data / 255 - imagenet_mean) / imagenet_stddev
+
+    # Add the batch dimension, as we are expecting 4-dimensional input: NCHW.
+    img_data = np.expand_dims(norm_img_data, axis=0)
+
+    target = tvm.target.cuda()
+
+    # The input name may vary across model types. You can use a tool
+    # like Netron to check input names
+    input_name = "data"
+    shape_dict = {input_name: img_data.shape}
+
+    mod, params = relay.frontend.from_onnx(onnx_model, shape_dict)
+
+    with tvm.transform.PassContext(opt_level=-1):
+        lib = relay.build(mod, target=target, params=params)
+
+    dev = tvm.device(str(target), 0)
+    module = graph_executor.GraphModule(lib["default"](dev))
+
+    source_module = lib.get_lib().imported_modules[0]
+
+    source_code = source_module.get_source()
+
+    with open("resnet50_source_code_cuda_base.txt", "a") as f:
+        print(source_code, file=f)
