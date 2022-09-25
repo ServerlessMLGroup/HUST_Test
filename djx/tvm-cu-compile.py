841e7b6daf2d105a8c34dd069cba933b377a8dd7
husterdjx <76643896@qq.com>
2022-09-25 09:39:02 +0800

    delete torch,use torch error

--- djx/tvm-cu-compile.py
+++ djx/tvm-cu-compile.py
@@ -1,8 +1,3 @@
-import onnx
-from tvm.contrib.download import download_testdata
-from PIL import Image
-import numpy as np
-import tvm.relay as relay
 from tvm.driver import tvmc
 import argparse
 
@@ -20,14 +15,7 @@ import os
 
 os.environ['CUDA_VISIBLE_DEVICES'] = ("%d" % args.gpu_no)
 
-import torch
-
 if __name__ == '__main__':
-    print("torch.cuda.device_count():", torch.cuda.device_count())
-    print("torch.cuda.current_device():", torch.cuda.current_device())
-    device = torch.device("cuda:%d" % gpu_no if torch.cuda.is_available() else "cpu")
-    print("device =", device)
-
     model = tvmc.load("/model/resnet50-v2-7.onnx") #Step 1: Load
 
     package = tvmc.compile(model, target="cuda", package_path="/cu")
