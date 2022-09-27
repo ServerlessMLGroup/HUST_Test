da99cf5441bc3112fae65c198c0f7e2b445b85a3
husterdjx <76643896@qq.com>
2022-09-27 10:57:44 +0800

    use own image

--- Dockerfile/install_tvm_gpu.sh
+++ Dockerfile/install_tvm_gpu.sh
@@ -19,6 +19,8 @@
 apt-get update
 apt-get install -y python3 python3-dev python3-setuptools gcc libtinfo-dev zlib1g-dev build-essential cmake libedit-dev libxml2-dev
 
+bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+
 cd /usr
 git clone https://github.com/apache/tvm tvm --recursive
 cd /usr/tvm
