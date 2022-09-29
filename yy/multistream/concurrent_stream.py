6be49fc84806bff2ebbc02bbb695419d9b42118a
hust-yxyhhh <yuyue18@hust.edu.cn>
2022-09-29 18:22:30 +0800

    later i don't want to change code both at local and remote

--- yy/multistream/concurrent_stream.py
+++ yy/multistream/concurrent_stream.py
@@ -44,40 +44,6 @@ def benchmark(log_file_name, worker_name, device, model, Stream, input_shape=(8,
         input_data = input_data.half()
 
     print("%s:Warm up ..." % worker_name)
-<<<<<<< HEAD
-    with torch.no_grad():
-        for _ in range(nwarmup):
-            features = model(input_data)
-    #torch.cuda.synchronize()
-    print("[%s]%s:Start timing ..." % (time.time(), worker_name))
-    timings = []
-    with torch.no_grad():
-        for i in range(1, nruns + 1):
-            start_time = time.time()
-            features = model(input_data)
-            #torch.cuda.synchronize()
-            end_time = time.time()
-            timings.append(end_time - start_time)
-            if i % 10 == 0:
-                # logger.info('Iteration %d/%d, %d-%d ave batch time %.2f ms' % (i, nruns, i, i - 10,
-                # np.mean(timings) * 1000))
-                print('%s:Iteration %d/%d, %d-%d ave batch time %.2f ms' % (
-                    worker_name, i, nruns, i, i - 10, np.mean(timings) * 1000))
-                log_file_handler.write('[%s] %s:Iteration %d/%d, %d-%d ave batch time %.2f ms\n' % (
-                    time.time(), worker_name, i, nruns, i, i - 10, np.mean(timings) * 1000))
-                timings.clear()
-    print("[%s]%s:End!--------" % (time.time(), worker_name))
-    log_file_handler.close()
-    # logger.info("Input shape:", input_data.size())
-    # print("Input shape:", input_data.size())
-    # logger.info("Output features size:", features.size())
-    # print("Output features size:", features.size())
-
-
-class WorkerProc(Process):
-    def __init__(self, name, start_pipe, mps_percentage, batch_size=32, nruns=500):
-        super(WorkerProc, self).__init__()
-=======
 
     with torch.cuda.stream(Stream):
         print("current stream: {}".format(torch.cuda.current_stream()))
@@ -112,7 +78,6 @@ class WorkerProc(Process):
 class WorkerThre(threading.Thread):
     def __init__(self, name, Stream,batch_size=32, nruns=500):
         super(WorkerThre, self).__init__()
->>>>>>> 0515e2d88d72f6a7624cb57a39b449295d4517d0
         self.name = name
         self.Stream = Stream
         # self.logger = log.get_logger(name, "torch-diff-mps")
@@ -146,26 +111,6 @@ class WorkerThre(threading.Thread):
 
 
 
-<<<<<<< HEAD
-    model4 = torch.hub.load('pytorch/vision:v0.10.0', 'resnet152', pretrained=True)
-    model4.to(device)
-    model4.eval()
-
-    s1 = torch.cuda.Stream()
-    s2 = torch.cuda.Stream()
-    s3 = torch.cuda.Stream()
-    s4 = torch.cuda.Stream()
-    default_stream = torch.cuda.current_stream()
-    print("Default Stream: {}".format(default_stream))
-    # 等待创建A的stream执行完毕
-    torch.cuda.Stream.synchronize(default_stream)
-
-    with torch.cuda.stream(s1):
-        print("current stream: {}".format(torch.cuda.current_stream()))
-        time.sleep(10)
-        benchmark(log_file_name="Stream-1", worker_name="Stream-1", device=device, model=model1,
-              input_shape=(batch_size, 3, 224, 224), nruns= 300)
-=======
         worker1.start()
         timestamp('Worker-1', 'start_compution')
         worker2.start()
@@ -174,7 +119,6 @@ class WorkerThre(threading.Thread):
         timestamp('Worker-3', 'start_compution')
         worker4.start()
         timestamp('Worker-4', 'start_compution')
->>>>>>> 0515e2d88d72f6a7624cb57a39b449295d4517d0
 
 
 
