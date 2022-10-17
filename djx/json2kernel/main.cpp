4eb65d798683bd7feaa5f77b031ab1c72da6a8a0
husterdjx <76643896@qq.com>
2022-10-17 17:53:07 +0800

    fix bug

--- djx/json2kernel/main.cpp
+++ djx/json2kernel/main.cpp
@@ -184,12 +184,13 @@ int main() {
     RETURN_STATUS(get_output(output));
     std::vector<float> ans = {0.0003392257, 0.0014304413, 0.0004299286, 0.0010349639, 0.0020997059,
                         0.0016049921, 0.0010267848, 0.00042607592, 0.0018747754, 0.0024558322};
-    for (size_t i = 0; i < ans.size(); i++)
+    for (size_t i = 0; i < ans.size(); i++) {
         // ASSERT_FLOAT_EQ(ans[i], output[i]);
-        if ans[i] != output[i] {
+        if (ans[i] != output[i]) {
             std::cout << "not match" << endl;
             exit(1);
         }
+    }
     printf("reset model!\n");
     model.reset();
     return 0;
