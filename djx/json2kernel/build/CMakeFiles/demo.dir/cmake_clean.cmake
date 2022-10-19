1f94145a345689470b330a719e58e8fba24a349f
hust-yxyhhh <yuyue18@hust.edu.cn>
2022-10-19 10:11:45 +0800

    generate a csv to see its format

---
+++ djx/json2kernel/build/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/json.cpp.o"
+  "CMakeFiles/demo.dir/json.cpp.o.d"
+  "CMakeFiles/demo.dir/log.cpp.o"
+  "CMakeFiles/demo.dir/log.cpp.o.d"
+  "CMakeFiles/demo.dir/main.cpp.o"
+  "CMakeFiles/demo.dir/main.cpp.o.d"
+  "CMakeFiles/demo.dir/model.cpp.o"
+  "CMakeFiles/demo.dir/model.cpp.o.d"
+  "demo"
+  "demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
