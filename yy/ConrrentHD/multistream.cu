defe6fdafec779049f9f613c70726a80b94b4c04
hust-yxyhhh <yuyue18@hust.edu.cn>
2022-11-01 09:34:21 +0800

    Multi Stream code test 1-3

--- yy/ConrrentHD/multistream.cu
+++ yy/ConrrentHD/multistream.cu
@@ -14,9 +14,9 @@
 using namespace std;
 
 //Mutex
-mutex mtx1-1;
-mutex mtx1-2;
-mutex mtx2-1;
+mutex mtx1_1;
+mutex mtx1_2;
+mutex mtx2_1;
 //clock_t
 clock_t start1,finish1;
 clock_t start2,finish2;
@@ -24,57 +24,57 @@ double singletime = 0.0;
 double cotime1=0.0;
 double cotime2=0.0;
 
-void CUDART_CB thread1-1callback(void *data) {
-    mtx1-1.lock();
+void CUDART_CB thread1_1callback(void *data) {
+    mtx1_1.lock();
     start1=clock();
 }
 
-void CUDART_CB thread1-2callback(void *data) {
+void CUDART_CB thread1_2callback(void *data) {
     finish1=clock();
-    singletime += (double)(finish1-start1)/CLOCKS_PER_SEC;
+    singletime += (double)(finish1 - start1)/CLOCKS_PER_SEC;
     cout<<"This time single data transfer: "<<((double)(finish1-start1)/CLOCKS_PER_SEC)<<"(s)"<<endl;
 }
 
-void CUDART_CB thread1-3callback(void *data) {
-    mtx2-1.unlock();
-    mtx1-2.lock();
+void CUDART_CB thread1_3callback(void *data) {
+    mtx2_1.unlock();
+    mtx1_2.lock();
     start1=clock();
 }
 
-void CUDART_CB thread1-4callback(void *data) {
+void CUDART_CB thread1_4callback(void *data) {
     finish1=clock();
     cotime1 += (double)(finish1-start1)/CLOCKS_PER_SEC;
     cout<<"This time cocurrent data transfer 1111: "<<((double)(finish1-start1)/CLOCKS_PER_SEC)<<"(s)"<<endl;
-    mtx1-1.unlock();
+    mtx1_1.unlock();
 }
-void CUDART_CB thread1-5callback(void *data) {
+void CUDART_CB thread1_5callback(void *data) {
     cout<<"single time: "<<singletime<<" s"<<endl;
     cout<<"cocurrent time1111: "<<cotime1<<" s"<<endl;
 }
 
-void CUDART_CB thread2-1callback(void *data) {
-    mtx2-1.lock();
+void CUDART_CB thread2_1callback(void *data) {
+    mtx2_1.lock();
     start2=clock();
 }
 
-void CUDART_CB thread2-2callback(void *data) {
+void CUDART_CB thread2_2callback(void *data) {
     finish2=clock();
-    cotime2 += (double)(finish1-start1)/CLOCKS_PER_SEC;
+    cotime2 += (double)(finish2-start2)/CLOCKS_PER_SEC;
     cout<<"This time cocurrent data transfer 2222: "<<((double)(finish2-start2)/CLOCKS_PER_SEC)<<"(s)"<<endl;
-    mtx1-2.unlock();
+    mtx1_2.unlock();
 }
 
-void CUDART_CB thread2-3callback(void *data) {
+void CUDART_CB thread2_3callback(void *data) {
     cout<<"cocurrent time2222: "<<cotime2<<" s"<<endl;
 }
 
 void thread1(cudaStream_t stream,float* d_a,float* d_b,float* h_a,float* h_b,size_t size)
 {
-    cudaHostFn_t fn1 = thread1-1callback;
-    cudaHostFn_t fn2 = thread1-2callback;
-    cudaHostFn_t fn3 = thread1-3callback;
-    cudaHostFn_t fn4 = thread1-4callback;
-    cudaHostFn_t fn5 = thread1-5callback;
+    cudaHostFn_t fn1 = thread1_1callback;
+    cudaHostFn_t fn2 = thread1_2callback;
+    cudaHostFn_t fn3 = thread1_3callback;
+    cudaHostFn_t fn4 = thread1_4callback;
+    cudaHostFn_t fn5 = thread1_5callback;
     for(int i=0;i < 10;i++)
     {
     cudaLaunchHostFunc(stream, fn1, null);
@@ -90,9 +90,9 @@ void thread1(cudaStream_t stream,float* d_a,float* d_b,float* h_a,float* h_b,siz
 
 void thread2(cudaStream_t stream,float* d_c,float* h_c,size_t size)
 {
-    cudaHostFn_t fn1 = thread2-1callback;
-    cudaHostFn_t fn2 = thread2-2callback;
-    cudaHostFn_t fn3 = thread2-3callback;
+    cudaHostFn_t fn1 = thread2_1callback;
+    cudaHostFn_t fn2 = thread2_2callback;
+    cudaHostFn_t fn3 = thread2_3callback;
     for(int i=0;i < 10;i++)
     {
     cudaLaunchHostFunc(stream, fn1, null);
@@ -142,7 +142,7 @@ int main()
     cudaStreamCreate(&secondstream);
 
     //prepare
-    mtx2-1.lock();
+    mtx2_1.lock();
     thread first=thread(thread1,firststream,d_A,d_B,h_A,h_B,size);
     thread second=thread(thread2,secondstream,d_C,h_C,size);
     second.join();
