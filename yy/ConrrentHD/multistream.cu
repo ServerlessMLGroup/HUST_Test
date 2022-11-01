eaf53b3432e16085dd9a4996409e65387d181d50
hust-yxyhhh <yuyue18@hust.edu.cn>
2022-11-01 09:42:22 +0800

    Multi Stream code test 1-6

--- yy/ConrrentHD/multistream.cu
+++ yy/ConrrentHD/multistream.cu
@@ -124,9 +124,9 @@ int main()
     cout<<"Allocate Host Memory"<<endl;
     // Allocate input vectors h_A and h_B in host memory
     float* h_A,h_B,h_C;
-    cudaMallocHost(h_A, size);
-    cudaMallocHost(h_B, size);
-    cudaMallocHost(h_C, size);
+    cudaMallocHost(&h_A, size);
+    cudaMallocHost(&h_B, size);
+    cudaMallocHost(&h_C, size);
 
     uniform_real_distribution<float> u(0,10);
     default_random_engine e(time(NULL));
