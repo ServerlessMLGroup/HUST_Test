bf59e188f9106807f2df69615c929d3c04b696b7
hust-yxyhhh <yuyue18@hust.edu.cn>
2022-11-22 20:50:39 +0800

    debug

--- yy/ptbkernel/ptbconvulution.cu
+++ yy/ptbkernel/ptbconvulution.cu
@@ -53,7 +53,7 @@ __global__ void convolutionkernel(float** photo,float**** temp,float** convoluti
 
     //caculate(COREX * COREY thread respectively by each thread)
 
-    temp[newy][newx][thy][thx] = photo[newy - (COREY-1)/2 + thy][newx - (COREX-1)/2 + thx] * convolutioncore[thy][thx];
+    //temp[newy][newx][thy][thx] = photo[newy - (COREY-1)/2 + thy][newx - (COREX-1)/2 + thx] * convolutioncore[thy][thx];
 
     __syncthreads();
 
@@ -66,7 +66,7 @@ __global__ void convolutionkernel(float** photo,float**** temp,float** convoluti
             }
         }
     }
-    
+
 }
 
 
