b248091342e8708f5e0f8662684d37700c8366de
hust-yxyhhh <yuyue18@hust.edu.cn>
2022-11-24 10:57:59 +0800

    check code

--- yy/ConrrentHD/threadExamine.cu
+++ yy/ConrrentHD/threadExamine.cu
@@ -94,10 +94,11 @@ void thread1(cudaStream_t stream,float* d_a,float* h_a,size_t size,long long uns
     //rotate for so many times,the total runtime didn't change
     for(int i=1;i < 11;i++)
     {
-    cudaMemcpyAsync(d_a, h_a,size, cudaMemcpyHostToDevice, tempstream);
+    //cudaMemcpyAsync(d_a, h_a,size, cudaMemcpyHostToDevice, tempstream);
     //old code
     //kernel_flager<<<1,1,0,tempstream>>>(i,flag);
     }
+    
 }
 
 int main()
