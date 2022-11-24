19582e9b50d11913ac4da557d3e570fe7aea80b6
hust-yxyhhh <yuyue18@hust.edu.cn>
2022-11-24 11:14:00 +0800

    check code

--- yy/ConrrentHD/threadExamine.cu
+++ yy/ConrrentHD/threadExamine.cu
@@ -80,7 +80,7 @@ void thread1(cudaStream_t stream,float* d_a,float* h_a,size_t size,long long uns
     cout << "In thread stream: "<<stream<<endl;
 
     //test whether the kernel worked ,it should work 67s,however,nothing happened
-    kernel<<<1,32,0,tempstream>>>(1.0,2.0,3.0,1000000000);
+    kernel<<<1,32>>>(1.0,2.0,3.0,1000000000);
     cudaError_t cudaStatus;
     cudaStatus = cudaGetLastError();
     if (cudaStatus != cudaSuccess) {
@@ -90,7 +90,7 @@ void thread1(cudaStream_t stream,float* d_a,float* h_a,size_t size,long long uns
     {
         fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
     }
-    
+
 
     //test wherther the parameter flag is effective,the line below worked well
     //flag[0] = 1;
