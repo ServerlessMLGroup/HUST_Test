202ee600fd79f49bd086b902beb06166485bcfa6
hust-yxyhhh <yuyue18@hust.edu.cn>
2022-11-29 20:09:42 +0800

    1129 temporary test

--- yy/ConrrentHD/mulStreamCoIo.cu
+++ yy/ConrrentHD/mulStreamCoIo.cu
@@ -72,7 +72,7 @@ int main()
     for(int i=0;i < 10;i++)
     {
     cudaMemcpyAsync(d_C, h_C,size, cudaMemcpyHostToDevice, secondstream);
-    cudaMemcpyAsync(d_B, h_B,size, cudaMemcpyHostToDevice, firststream);
+    cudaMemcpyAsync(d_B, h_B,size/2, cudaMemcpyHostToDevice, firststream);
     }
 
     for(int i=0;i < 10;i++)
