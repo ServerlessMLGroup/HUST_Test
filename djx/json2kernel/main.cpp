3834a8ed05788c18bd648309685b674486fdcddb
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-01-08 10:22:21 +0800

    Module load test 4

--- djx/json2kernel/main.cpp
+++ djx/json2kernel/main.cpp
@@ -25,8 +25,6 @@ void thread1(CUcontext ctx)
     }
 
    CUmodule mod1,mod2,mod3,mod4,mod5,mod6;
-
-   GPU_RETURN_STATUS(cuModuleLoad(&mod, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx"));
    GPU_RETURN_STATUS(cuModuleLoad(&mod1, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx"));
    GPU_RETURN_STATUS(cuModuleLoad(&mod2, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx"));
    GPU_RETURN_STATUS(cuModuleLoad(&mod3, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx"));
