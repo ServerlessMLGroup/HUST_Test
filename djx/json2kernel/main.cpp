2e31bdb3b4d04dc90f21203d3277d569fe56a249
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-01-08 10:23:38 +0800

    Module load test 4

--- djx/json2kernel/main.cpp
+++ djx/json2kernel/main.cpp
@@ -25,12 +25,12 @@ void thread1(CUcontext ctx)
     }
 
    CUmodule mod1,mod2,mod3,mod4,mod5,mod6;
-   GPU_RETURN_STATUS(cuModuleLoad(&mod1, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx"));
-   GPU_RETURN_STATUS(cuModuleLoad(&mod2, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx"));
-   GPU_RETURN_STATUS(cuModuleLoad(&mod3, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx"));
-   GPU_RETURN_STATUS(cuModuleLoad(&mod4, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx"));
-   GPU_RETURN_STATUS(cuModuleLoad(&mod5, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx"));
-   GPU_RETURN_STATUS(cuModuleLoad(&mod6, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx"));
+   cuModuleLoad(&mod1, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx");
+   cuModuleLoad(&mod2, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx");
+   cuModuleLoad(&mod3, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx");
+   cuModuleLoad(&mod4, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx");
+   cuModuleLoad(&mod5, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx");
+   cuModuleLoad(&mod6, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx");
 }
 // add fininshed
 
