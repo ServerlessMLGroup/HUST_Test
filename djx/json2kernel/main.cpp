66c6973f0c193ed4933755b07c4c516521169918
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-01-12 16:01:54 +0800

    some change

--- djx/json2kernel/main.cpp
+++ djx/json2kernel/main.cpp
@@ -318,6 +318,7 @@ int main(int argc, char **argv) {
         //auto &array = params->at(storage_info.name);
         //yy change
         array=params->mpdata->at(storage_info.name);
+        float* temp =array;
         size=params->mpsize->at(storage_info.name);
 
         //old
@@ -327,7 +328,7 @@ int main(int argc, char **argv) {
 
         //yychange
         tempsize = size*sizeof(float);
-        cudaMemcpyAsync((CUdeviceptr)storage[i], (void*)array, tempsize, cudaMemcpyHostToDevice, firststream);
+        cudaMemcpyAsync((CUdeviceptr)storage[i], array, tempsize, cudaMemcpyHostToDevice, firststream);
 
         //std::cout<<model->kernels[i].name.c_str()<<" size: "<<array.size() * sizeof(float)<<" byte"<<std::endl;
 	    //std::cout<<model->kernels[i].name.c_str()<<" time: "<<1000*time<<" us"<<std::endl;
