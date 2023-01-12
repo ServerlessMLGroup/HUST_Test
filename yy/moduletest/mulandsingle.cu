de18c1c97ea854fd43728925bb256afb4c64c254
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-01-12 20:00:48 +0800

    check context change

--- yy/moduletest/mulandsingle.cu
+++ yy/moduletest/mulandsingle.cu
@@ -125,8 +125,9 @@ int main()
    std::cout<<"Size now"<<now<<std::endl;
 
    CUmodule mod1,mod2,mod3,mod4,mod5,mod6;
-   cuModuleLoad(&mod1, "/home/wuhao/HUST_Test/yy/moduletest/temp1.ptx");
-  
+
+   cuModuleLoad(&mod2, "/home/wuhao/HUST_Test/yy/moduletest/temp2.ptx");
+   
    cudaMemGetInfo(&now,&total);
 
    std::cout<<"Size now"<<now<<std::endl;
