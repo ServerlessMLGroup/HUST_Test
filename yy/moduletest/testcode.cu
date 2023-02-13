60b5ac6653e0daf1c06ac0cffa32730d8ab0dc61
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-02-13 11:45:04 +0800

    module test code test

--- yy/moduletest/testcode.cu
+++ yy/moduletest/testcode.cu
@@ -184,8 +184,8 @@ int main()
     float* cpudata;
     CUdeviceptr* gpudata;
     size_t size = 5*1024*1024;
-    cuMemAllocHost((void**)(&cpudata),size);
-    cuMemAlloc(gpudata, size);
+    //cuMemAllocHost((void**)(&cpudata),size);
+    //cuMemAlloc(gpudata, size);
     /*
     for(int i=0;i<5*1024*1024;i++)
     {
