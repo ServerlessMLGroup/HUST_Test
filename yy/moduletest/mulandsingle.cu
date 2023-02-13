cba82017b9e4f0460678d771001471e2630afec7
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-02-13 10:46:18 +0800

    module test new start

--- yy/moduletest/mulandsingle.cu
+++ yy/moduletest/mulandsingle.cu
@@ -128,7 +128,9 @@ int main()
 
    cuModuleLoad(&mod2, "/home/wuhao/HUST_Test/yy/moduletest/temp2.ptx");
    cuModuleLoad(&mod3, "/home/wuhao/HUST_Test/yy/moduletest/temp3.ptx");
- 
+   cuModuleLoad(&mod4, "/home/wuhao/HUST_Test/yy/moduletest/temp4.ptx");
+   cuModuleLoad(&mod5, "/home/wuhao/HUST_Test/yy/moduletest/temp7.ptx");
+   cuModuleLoad(&mod6, "/home/wuhao/HUST_Test/yy/moduletest/temp6.ptx");
    cudaMemGetInfo(&now,&total);
 
    std::cout<<"Size now"<<now<<std::endl;
