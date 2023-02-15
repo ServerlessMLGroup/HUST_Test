ddbb99214955e432ebb35c67779e74bfa7033ffb
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-02-15 10:05:28 +0800

    cache different device

--- yy/moduletest/testcode.cu
+++ yy/moduletest/testcode.cu
@@ -146,7 +146,7 @@ void thread2(CUcontext ctx)
 int main()
 {
     cuInit(0);
-    cudaSetDevice(2);
+    cudaSetDevice(3);
 
 
     //1.create context
@@ -207,7 +207,7 @@ int main()
     //std::cout<<"Size now"<<now<<std::endl;
 
     CUmodule mod1,mod2,mod3,mod4,mod5,mod6;
-    cuModuleLoad(&mod1, "/home/wuhao/HUST_Test/yy/moduletest/temp3.ptx");
+    cuModuleLoad(&mod1, "/home/wuhao/HUST_Test/yy/moduletest/temp4.ptx");
     /*
     cuModuleLoad(&mod2, "/home/wuhao/HUST_Test/yy/moduletest/temp2.ptx");
     cuModuleLoad(&mod3, "/home/wuhao/HUST_Test/yy/moduletest/temp3.ptx");
