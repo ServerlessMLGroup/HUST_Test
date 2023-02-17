66c614baaea6ec0a95285f59c589df63fddb19e1
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-02-17 11:03:49 +0800

    concurrent thread

--- yy/moduletest/testcode.cu
+++ yy/moduletest/testcode.cu
@@ -106,7 +106,7 @@ void thread1(CUcontext ctx)
    mtx1_2.unlock();
    */
    cuModuleLoad(&mod1, "/home/wuhao/HUST_Test/yy/moduletest/temp1.ptx");
-   mtex1_2.unlock();
+   mte1_2.unlock();
    /*
    cuModuleLoad(&mod2, "/home/wuhao/HUST_Test/yy/moduletest/temp2.ptx");
    cuModuleLoad(&mod3, "/home/wuhao/HUST_Test/yy/moduletest/temp3.ptx");
