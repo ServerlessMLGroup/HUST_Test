a7fbf96df0ac58e6cdc0ed47e164a9b3272074bb
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-09 11:08:21 +0800

    again and again

--- yy/lunwentest/memmanaged.cu
+++ yy/lunwentest/memmanaged.cu
@@ -143,13 +143,13 @@ int main()
     cudaDeviceSynchronize();
     */
 
-
+    /*
     i=cudaMemPrefetchAsync(h_A,storage_size,2);
     if(i)
     {
     cout<<"prefetch error: "<<i<<endl;
     }
-    
+    */
     VecAdd<<<100,100>>>(h_A,1.0);
     getMem();
 
