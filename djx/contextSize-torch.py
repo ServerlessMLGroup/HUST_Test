465485e2797fa02f3383dabbac7032f3c636565a
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-09 17:15:01 +0800

    bu

--- djx/contextSize-torch.py
+++ djx/contextSize-torch.py
@@ -19,8 +19,8 @@ print("before import torch:")
 os.system("./getSize")
 
 import torch
-torch.backends.cudnn.enabled = True
-torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.enabled = False
+torch.backends.cudnn.benchmark = False
 
 if __name__ == '__main__':
     device = torch.device("cuda:%d" % gpu_no if torch.cuda.is_available() else "cpu")
