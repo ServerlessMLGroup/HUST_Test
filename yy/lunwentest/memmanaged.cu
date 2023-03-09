1e85f7f85474c0583a5ac89191f73e5a74d24570
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-09 09:43:05 +0800

    again and again

--- yy/lunwentest/memmanaged.cu
+++ yy/lunwentest/memmanaged.cu
@@ -86,8 +86,17 @@ int main()
     size_t storage_size = 1048576*2000;
     float* h_A;
     cudaMallocHost(&h_A, storage_size);
+
+    //nan dao cu driver api bu xing?
+    /*
     CUdeviceptr device_ptr;
     int i=cuMemAlloc((CUdeviceptr*)&device_ptr, storage_size);
+    */
+    float* device_ptr;
+    cudaMalloc(&device_ptr,storage_size);
+
+
+
     if(i)
     {
     cout<<"error: "<<i<<endl;
