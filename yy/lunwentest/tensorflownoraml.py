4ae38ec5d886989b0a69f21834dfd7a71fc52963
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-10 17:47:20 +0800

    test cudnn

--- yy/lunwentest/tensorflownoraml.py
+++ yy/lunwentest/tensorflownoraml.py
@@ -1,12 +1,10 @@
 import tensorflow as tf
 
-# 通过tf.device将运算指定到特定的设备上。
-with tf.device('/cpu:0'):
-    a = tf.constant([1.0,2.0,3.0], shape=[3], name='a')
-    b = tf.constant([1.0,2.0,3.0], shape=[3], name='b')
+a = tf.constant([1.0, 2.0, 3.0], shape=[3], name='a')
+b = tf.constant([1.0, 2.0, 3.0], shape=[3], name='b')
+c = a + b
 
-with tf.device('/gpu:1'):
-    c = a + b
-
-sess = tf.Session(config=tf.ConfigProto(log_device_palcement = True))
-print sess.run(c)
+# 通过log_device_placement参数来输出运行每一个运算的设备。
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+print
+sess.run(c)
