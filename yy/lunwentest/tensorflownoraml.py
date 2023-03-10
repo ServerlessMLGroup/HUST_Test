3f8cbfbc4d7ff7d1a8adbe858001d8054856afc5
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-10 18:31:13 +0800

    test cudnn

--- yy/lunwentest/tensorflownoraml.py
+++ yy/lunwentest/tensorflownoraml.py
@@ -8,6 +8,6 @@ os.system("./get2Size")
 # 通过log_device_placement参数来输出运行每一个运算的设备。
 sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 print
-os.system("./getSize")
+os.system("./get2Size")
 sess.run(c)
 os.system("./get2Size")
