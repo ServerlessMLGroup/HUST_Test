b30716655ff7d38e18e8a6db46b392d4c439821c
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-10 18:21:12 +0800

    test cudnn

--- yy/lunwentest/tensorflownoraml.py
+++ yy/lunwentest/tensorflownoraml.py
@@ -5,6 +5,6 @@ b = tf.constant([1.0, 2.0, 3.0], shape=[3], name='b')
 c = a + b
 
 # 通过log_device_placement参数来输出运行每一个运算的设备。
-sess = tf.compat.v1.Session(config=tf.ConfigProto(log_device_placement=True))
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
 print
 sess.run(c)
