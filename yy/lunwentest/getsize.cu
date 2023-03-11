3e7dc47740888c2f4e4a5b97592ea66ab0ee4c2b
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-11 13:10:29 +0800

    test cudnn

---
+++ yy/lunwentest/getsize.cu
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <math.h>
+#include <cuda.h>
+#include "cuda_runtime.h"
+#include <thread>
+#include <random>
+#include <ctime>
+using namespace std;
+
+enum class Unit{
+    Byte, KB, MB, GB, TB, PB, EB
+};
+
+
+double convert(double size, Unit unit)
+{
+    double result = size;
+    switch (unit)
+    {
+    case Unit::EB:
+        result /= 1024;     // flow through
+    case Unit::PB:
+        result /= 1024;     // flow through
+    case Unit::TB:
+        result /= 1024;     // flow through
+    case Unit::GB:
+        result /= 1024;     // flow through
+    case Unit::MB:
+        result /= 1024;     // flow through
+    case Unit::KB:
+        result /= 1024;     // flow through
+    case Unit::Byte:
+        result /= 1;
+    default:
+        break;
+    }
+    return result;
+}
+
+void getMem() {
+    size_t free, total;
+    int err=cudaMemGetInfo(&free, &total);
+    if(err){
+       cout<<"cudaMemGetInfo error:"<<err<<endl;
+       return;
+    }
+    printf("Free mem = %.4f MB, Total = %.4f MB \n\n\n\n", convert(free, Unit::MB), convert(total, Unit::MB));
+}
+
+int main() {
+    cudaSetDevice(2);
+    getMem();
+    return 0;
+}
