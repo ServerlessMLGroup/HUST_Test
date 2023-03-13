70082144075ba32fc75e68109324b3ade08a9736
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-13 10:10:53 +0800

    test

--- yy/lunwentest/tensorf.py
+++ yy/lunwentest/tensorf.py
@@ -4,6 +4,7 @@ import os
 tf.debugging.set_log_device_placement(True)
 os.system("./get2Size")
 #Place tensors on the GPU
+#下面这行会引起如下图一至图N一系列操作
 with tf.device('/GPU:2'):
     a=tf.constant([[1.0,2.0,3.0],[4.0,5.0,6.0]])
     b=tf.constant([[1.0,2.0],[3.0,4.0],[5.0,6.0]])
