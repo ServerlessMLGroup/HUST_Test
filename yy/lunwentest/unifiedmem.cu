3bcc93674edccf34923200616c7676962f3d4469
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-14 15:20:05 +0800

    Unified mem

--- yy/lunwentest/unifiedmem.cu
+++ yy/lunwentest/unifiedmem.cu
@@ -94,9 +94,9 @@ int main()
 
     //1048576 -> 1M
     //size_t storage_size = 1048576*400;
-    size_t storage_size = 1500*1048576;
+    size_t storage_size = 1000*1048576;
 
-    storage_size *=20;
+    storage_size *=40;
 
     cout<<"storage_size: "<<storage_size<<endl;
     cout<<"size of size_t: "<<sizeof(size_t)<<endl;
