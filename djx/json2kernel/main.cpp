7e3f8b5bf0aec147eefbc1e4b0e1f5e9fc12df46
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-16 11:17:56 +0800

    all test again

--- djx/json2kernel/main.cpp
+++ djx/json2kernel/main.cpp
@@ -12,7 +12,7 @@
 #include "unistd.h"
 #include <thread>
 
-#define BLOCKNUMBER 32000
+#define BLOCKNUMBER 32
 
 // #include <glog/logging.h>
 //Notice
@@ -325,6 +325,15 @@ int main(int argc, char **argv) {
         CUfunction func = kernels[func_name];
         uint32_t *launch_params = kernel_info.launch_params;
 
+        if(j==47)
+        {
+         std::cout<<"name"<<func_name<<std::endl;
+        std::cout<<"0 "<<launch_params[0]<<std::endl;
+        std::cout<<"1 "<<launch_params[1]<<std::endl;
+        std::cout<<"2 "<<launch_params[2]<<std::endl;
+        //continue;
+        }
+
         if(launch_params[0]*launch_params[1]*launch_params[2]>BLOCKNUMBER)
         {
         GPU_RETURN_STATUS(cuLaunchKernel(func,
@@ -332,13 +341,11 @@ int main(int argc, char **argv) {
         launch_params[3], launch_params[4], launch_params[5],
         0, kefirststream, (void **)raw_args1[j].data(), 0 // raw_args1是json中指示的storage的下标
     ));
-        /*
         GPU_RETURN_STATUS(cuLaunchKernel(func,
         BLOCKNUMBER, 1, 1,
         launch_params[3], launch_params[4], launch_params[5],
         0, kesecondstream, (void **)raw_args2[j].data(), 0 // raw_args1是json中指示的storage的下标
     ));
-        */
         }
         else{
         GPU_RETURN_STATUS(cuLaunchKernel(func,
@@ -346,20 +353,18 @@ int main(int argc, char **argv) {
         launch_params[3], launch_params[4], launch_params[5],
         0, kefirststream, (void **)raw_args1[j].data(), 0 // raw_args1是json中指示的storage的下标
     ));
-        /*
         GPU_RETURN_STATUS(cuLaunchKernel(func,
         launch_params[0], launch_params[1], launch_params[2],
         launch_params[3], launch_params[4], launch_params[5],
         0, kesecondstream, (void **)raw_args2[j].data(), 0 // raw_args1是json中指示的storage的下标
     ));
         }
-        */
+
         j++;
 
     }
-    }
 
-    //cuStreamSynchronize(kesecondstream);
+    cuStreamSynchronize(kesecondstream);
     cuStreamSynchronize(kefirststream);
 
     //std::vector<float> output(1000);
