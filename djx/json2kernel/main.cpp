3a044f70c4bdf3dd919caf1d673a137edc2580f0
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-16 15:19:00 +0800

    pipe test

--- djx/json2kernel/main.cpp
+++ djx/json2kernel/main.cpp
@@ -10,7 +10,10 @@
 #include <thread>
 #include <math.h>
 #include "unistd.h"
-#include <thread>
+#include <mutex>
+
+#define BLOCKNUMBER 32000
+
 // #include <glog/logging.h>
 //Notice
 // To make some experiments, i(yy) make some changes here. Before changing, i copied all the code
@@ -40,9 +43,18 @@ enum Status {
 }
 //old
 std::vector<CUdeviceptr> storage;
+//std::vector<CUdeviceptr> storage1;
+std::vector<CUdeviceptr> storage2;
 std::unordered_map<std::string, CUfunction> kernels;
-std::vector<std::vector<CUdeviceptr*>> raw_args;
+//std::unordered_map<std::string, CUfunction> kernels1;
+//std::unordered_map<std::string, CUfunction> kernels2;
+//std::vector<std::vector<CUdeviceptr*>> raw_args1;
+std::vector<std::vector<CUdeviceptr*>> raw_args2;
 std::unique_ptr<Model> model;
+
+std::mutex workend2;
+std::mutex workend1;
+
 Status launch_kernel(int kernel_offset, CUstream stream, Model* model) {
     int i = kernel_offset;
     std::string& func_name = model->kernels[i].name;
@@ -52,11 +64,11 @@ Status launch_kernel(int kernel_offset, CUstream stream, Model* model) {
     GPU_RETURN_STATUS(cuLaunchKernel(func,
         launch_params[0], launch_params[1], launch_params[2],
         launch_params[3], launch_params[4], launch_params[5],
-        0, stream, (void **)raw_args[i].data(), 0 // raw_args是json中指示的storage的下标
+        0, stream, (void **)raw_args2[i].data(), 0 // raw_args1是json中指示的storage的下标
     ));
     // double duration = (double(end - start));
     // std::cout << "func_name:" << func_name << " time:" << duration << std::endl;
-    // std::cout << "func_name:" << func_name << " launch_params:" << launch_params[0] << " " << launch_params[1] << " " << launch_params[2] << " " << launch_params[3] << " " << launch_params[4] << " " << launch_params[5] << " raw_args_ptr:" << (void **)raw_args[i].data() << std::endl;
+    // std::cout << "func_name:" << func_name << " launch_params:" << launch_params[0] << " " << launch_params[1] << " " << launch_params[2] << " " << launch_params[3] << " " << launch_params[4] << " " << launch_params[5] << " raw_args1_ptr:" << (void **)raw_args1[i].data() << std::endl;
     return Status::Succ;
 }
 Status execute_to(int idx, CUstream stream, Model* model) {
@@ -137,13 +149,16 @@ bool argexist(int temparg,int* aused,int* top)
     *top=*top +1;
     return false;
 }
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("args num error! argc:%d", argc);
-    }
-    int gpu_no = atoi(argv[1]);
-    log("preate unique_ptr");
-    model.reset(Model::from_json("/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18-final.json"));
+
+void thread1(int gpu_no,int i)
+{
+    std::cout<<"thread starts: "<<i<<std::endl;
+    std::unordered_map<std::string, CUfunction> kernels1;
+    std::vector<std::vector<CUdeviceptr*>> raw_args1;
+    std::vector<CUdeviceptr> storage1;
+    std::unique_ptr<Model> model1;
+
+    model1.reset(Model::from_json("/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18-final.json"));
     CUcontext ctx;
     CUdevice device;
     CUresult result;
@@ -155,52 +170,72 @@ int main(int argc, char **argv) {
     GPU_RETURN_STATUS(cuModuleLoad(&mod, "/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.ptx"));
     printf("load cuda kernels!\n");
     //yy add stream
-    CUstream firststream;
-    cuStreamCreate(&firststream,0);
-    CUstream secondstream;
-    cuStreamCreate(&secondstream,0);
-    //add fininshed
+    CUstream iofirststream;
+    cuStreamCreate(&iofirststream,0);
+    CUstream kefirststream;
+    cuStreamCreate(&kefirststream,0);
+
     // 2. load cuda kernels
-    for (KernelInfo &kernel_info : model->kernels) {
+    for (KernelInfo &kernel_info : model1->kernels) {
         CUfunction kernel;
         GPU_RETURN_STATUS(
             cuModuleGetFunction(&kernel, mod, kernel_info.name.c_str())
         );
-        kernels.emplace(kernel_info.name, kernel);
+        kernels1.emplace(kernel_info.name, kernel);
     }
+
     printf("allocate device storage!\n");
     // 3. allocate device storage
-    for (StorageInfo &storage_info : model->storage) {
+    for (StorageInfo &storage_info : model1->storage) {
         size_t stype_size = Model::get_stype_size(storage_info.stype);
         size_t storage_size = stype_size * storage_info.size;
-        CUdeviceptr device_ptr;
-        //std::vector<char> temp;
-        //temp.resize(storage_size, 0);
-        GPU_RETURN_STATUS(cuMemAlloc((CUdeviceptr*)&device_ptr, storage_size));
-        //GPU_RETURN_STATUS(cuMemcpyHtoD(device_ptr, temp.data(), storage_size));
-        storage.push_back(device_ptr);
+        CUdeviceptr device_ptr1;
+        GPU_RETURN_STATUS(cuMemAlloc((CUdeviceptr*)&device_ptr1, storage_size));
+        storage1.push_back(device_ptr1);
     }
+
     printf("map raw args!\n");
-    std::cout << "storages.size = " << storage.size() << std::endl;
-    raw_args.reserve(model->kernels.size());
-    for (KernelInfo &kernel_info : model->kernels) {
-        std::vector<CUdeviceptr*> kernel_arg;
-        for (size_t arg_idx : kernel_info.args) {
+    std::cout << "storages.size = " << storage1.size() << std::endl;
+    raw_args1.reserve(model1->kernels.size());
+
+    CUdeviceptr device_ptr11[model1->kernels.size()];
+    CUdeviceptr device_ptr12[model1->kernels.size()];
+    CUdeviceptr device_ptr13[model1->kernels.size()];
+
+    for (int i=0;i<model1->kernels.size();i++) {
+        std::vector<CUdeviceptr*> kernel_arg1;
+        //flag
+        //CUdeviceptr device_ptr1;
+        size_t storage_size = 1 * sizeof(int);
+        GPU_RETURN_STATUS(cuMemAlloc((CUdeviceptr*)(&(device_ptr11[i])), storage_size));
+        kernel_arg1.push_back(&(device_ptr11[i]));
+        //block num
+        //CUdeviceptr device_ptr2;
+        storage_size = 3 * sizeof(int);
+        GPU_RETURN_STATUS(cuMemAlloc((CUdeviceptr*)(&(device_ptr12[i])), storage_size));
+        kernel_arg1.push_back(&(device_ptr12[i]));
+        //blocksize
+        //CUdeviceptr device_ptr3;
+        storage_size = 1 * sizeof(int);
+        GPU_RETURN_STATUS(cuMemAlloc((CUdeviceptr*)(&(device_ptr13[i])), storage_size));
+        kernel_arg1.push_back(&(device_ptr13[i]));
+        for (size_t arg_idx : model1->kernels[i].args) {
             // assert(arg_idx < storage.size());
-            kernel_arg.push_back(&storage[arg_idx]);
+            kernel_arg1.push_back(&storage1[arg_idx]);
         }
-        raw_args.push_back(kernel_arg);
+        raw_args1.push_back(kernel_arg1);
     }
+
     printf("parse params!\n");
     parseresult* params = ModelParamParser::parse_from_file("/home/wuhao/HUST_Test/djx/json2kernel/resource/resnet18.param");
     std::cout<<" test 2: "<<std::endl;
     int kernel_offset=0;
     float* temp[80];
     size_t evsize[80];
-    for (KernelInfo &kernel_info : model->kernels) {
+    for (KernelInfo &kernel_info : model1->kernels) {
         for (size_t arg_idx : kernel_info.args) {
           //zhe li shao yi ge chuan di
-          StorageInfo& storage_info = model->storage[arg_idx];
+          StorageInfo& storage_info = model1->storage[arg_idx];
         if (params->mpdata->find(storage_info.name) == params->mpdata->end())
             continue;
         temp[kernel_offset]=params->mpdata->at(storage_info.name);
@@ -212,59 +247,97 @@ int main(int argc, char **argv) {
     kernel_offset=0;
     int j=0;
     float* temp2;
-
-    //create event
-    CUevent events[80];
-    for(int i=0;i<80;i++)
-    {
-    cuEventCreate(&events[i],0);
-    }
-
-
-    RETURN_STATUS(set_input());
-    for (KernelInfo &kernel_info : model->kernels) {
+    //RETURN_STATUS(set_input());
+    for (KernelInfo &kernel_info : model1->kernels) {
         for (size_t arg_idx : kernel_info.args) {
           //zhe li shao yi ge chuan di
-          StorageInfo& storage_info = model->storage[arg_idx];
+          StorageInfo& storage_info = model1->storage[arg_idx];
           if(params->mpdata->find(storage_info.name) == params->mpdata->end())
             continue;
-          GPU_RETURN_STATUS(cuMemcpyHtoDAsync((CUdeviceptr)storage[arg_idx],temp[kernel_offset], evsize[kernel_offset],firststream));
+          GPU_RETURN_STATUS(cuMemcpyHtoDAsync((CUdeviceptr)storage1[arg_idx],temp[kernel_offset], evsize[kernel_offset],iofirststream));
           kernel_offset++;
         }
-        //pipe
-        //cuStreamSynchronize(firststream);
+    }
 
-        cuEventRecordWithFlags(events[j],firststream,0);
-        cuStreamWaitEvent(secondstream,events[j],0);
+    //init flag
+    int* allflag;
+    cuMemAllocHost((void**)(&allflag), 80*sizeof(int));
+    for(int i=0;i<80;i++)
+    {
+    allflag[i]=1;
+    }
+
+    //init blocksize
+    int* allblocksize;
+    cuMemAllocHost((void**)(&allblocksize), 80*sizeof(int));
+    for(int i=0;i<80;i++)
+    {
+    allblocksize[i]=BLOCKNUMBER;
+    }
+    //init blocknum
+    int* allblocknum;
+    cuMemAllocHost((void**)(&allblocknum), 240*sizeof(int));
+    for(int i=0;i<model1->kernels.size();i++)
+    {
+        uint32_t *launch_params = model1->kernels[i].launch_params;
+        allblocknum[3*i+0]=launch_params[0];
+        allblocknum[3*i+1]=launch_params[1];
+        allblocknum[3*i+2]=launch_params[2];
+        GPU_RETURN_STATUS(cuMemcpyHtoDAsync((CUdeviceptr)(device_ptr11[i]),(allflag+i),1*sizeof(int),iofirststream));
+        GPU_RETURN_STATUS(cuMemcpyHtoDAsync((CUdeviceptr)(device_ptr12[i]),(allblocknum+3*i),3*sizeof(int),iofirststream));
+        GPU_RETURN_STATUS(cuMemcpyHtoDAsync((CUdeviceptr)(device_ptr13[i]),(allblocksize+i),1*sizeof(int),iofirststream));
+    }
+
+    cuStreamSynchronize(iofirststream);
+
+    if(i==1)
+    {
+    workend2.unlock();
+    workend1.lock();
+    }
+    else
+    {
+    workend2.lock();
+    workend1.unlock();
+    }
+
+    for (KernelInfo &kernel_info : model1->kernels) {
         std::string& func_name = kernel_info.name;
-        CUfunction func = kernels[func_name];
+        CUfunction func1 = kernels1[func_name];
         uint32_t *launch_params = kernel_info.launch_params;
-        GPU_RETURN_STATUS(cuLaunchKernel(func,
+        if(launch_params[0]*launch_params[1]*launch_params[2]>BLOCKNUMBER)
+        {
+        GPU_RETURN_STATUS(cuLaunchKernel(func1,
+        BLOCKNUMBER, 1, 1,
+        launch_params[3], launch_params[4], launch_params[5],
+        0, kefirststream, (void **)raw_args1[j].data(), 0 // raw_args1是json中指示的storage的下标
+    ));
+        }
+        else{
+        GPU_RETURN_STATUS(cuLaunchKernel(func1,
         launch_params[0], launch_params[1], launch_params[2],
         launch_params[3], launch_params[4], launch_params[5],
-        0, secondstream, (void **)raw_args[j].data(), 0 // raw_args是json中指示的storage的下标
+        0, kefirststream, (void **)raw_args1[j].data(), 0 // raw_args1是json中指示的storage的下标
     ));
-        j++;
-
-    }
-    cuStreamSynchronize(secondstream);
-
-    /*
-    cuStreamSynchronize(firststream);
-    j=0;
-    for (KernelInfo &kernel_info : model->kernels) {
-        std::string& func_name = kernel_info.name;
-        CUfunction func = kernels[func_name];
-        uint32_t *launch_params = kernel_info.launch_params;
-        GPU_RETURN_STATUS(cuLaunchKernel(func,
-        launch_params[0], launch_params[1], launch_params[2],
-        launch_params[3], launch_params[4], launch_params[5],
-        0, secondstream, (void **)raw_args[j].data(), 0 // raw_args是json中指示的storage的下标
-    ));
+        }
         j++;
     }
-    cuStreamSynchronize(secondstream);
-    */
+    cuStreamSynchronize(kefirststream);
+    sleep(1);
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("args num error! argc:%d", argc);
+    }
+
+    workend1.lock();
+    workend2.lock();
+    std::thread first(thread1,2,1);
+    std::thread second(thread1,2,2);
+    first.join();
+    second.join();
+
 
     //std::vector<float> output(1000);
     // RETURN_STATUS(get_output(output));
@@ -274,6 +347,7 @@ int main(int argc, char **argv) {
     //     // ASSERT_FLOAT_EQ(ans[i], output[i]);
     //     std::cout << output[i] << " vs " << ans[i] << std::endl;
     // }
+
     printf("reset model!\n");
     model.reset();
     return 0;
