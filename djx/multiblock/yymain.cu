d95e6ea42ca62018d545a4ea50696ff490e13323
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-22 19:44:01 +0800

    kernel problem

--- djx/multiblock/yymain.cu
+++ djx/multiblock/yymain.cu
@@ -84,7 +84,7 @@ extern "C" __global__ void fused_nn_conv2d_add_multiply_add_nn_relu_kernel0(int 
             if(atomicAdd(sm_flag + smid, 1)< WORKER_NUM_PERSM)
             {
             basicoffset = WORKER_NUM_PERSM*(smid-(number-1)*SM_NUM) + sm_flag[smid]-1;
-            printf("smid %d\n", smid);
+            //printf("smid %d\n", smid);
             }
        }
     }
