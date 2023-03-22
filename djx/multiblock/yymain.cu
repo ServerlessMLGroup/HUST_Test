8688190379f92ae8ac15eeae42de92f403fd59c4
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-22 18:51:16 +0800

    kernel problem

--- djx/multiblock/yymain.cu
+++ djx/multiblock/yymain.cu
@@ -580,8 +580,8 @@ int main(int argc, char *argv[]) {
 
 
     // launch kernel
-    //fused_nn_conv2d_add_multiply_add_nn_relu_kernel0<<<Dim_block, Dim_thread, 0, streams[0]>>>(1,g_flag, g_ph0, g_ph1, g_ph2, g_ph3, g_ph4, g_ph5);
-    fused_nn_conv2d_add_multiply_add_nn_relu_kernel0<<<Dim_block, Dim_thread, 0, streams[1]>>>(2,g_flag_, g_ph0_, g_ph1_, g_ph2_, g_ph3_, g_ph4_, g_ph5_);
+    fused_nn_conv2d_add_multiply_add_nn_relu_kernel0<<<Dim_block, Dim_thread, 0, streams[0]>>>(1,g_flag, g_ph0, g_ph1, g_ph2, g_ph3, g_ph4, g_ph5);
+    //fused_nn_conv2d_add_multiply_add_nn_relu_kernel0<<<Dim_block, Dim_thread, 0, streams[1]>>>(2,g_flag_, g_ph0_, g_ph1_, g_ph2_, g_ph3_, g_ph4_, g_ph5_);
 
     cudaDeviceSynchronize();
 
