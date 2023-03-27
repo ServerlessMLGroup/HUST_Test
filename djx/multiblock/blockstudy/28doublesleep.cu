216bfd444468e35eef89fcf29f7216c98f023aae
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-27 11:16:03 +0800

    block study 1

--- djx/multiblock/blockstudy/28doublesleep.cu
+++ djx/multiblock/blockstudy/28doublesleep.cu
@@ -86,6 +86,15 @@ extern "C" __global__ void fused_nn_conv2d_add_multiply_add_nn_relu_kernel0(int 
        int blocknumber=atomicAdd(sm_flag + smid, 1);
        atomicAdd(worker + smid, 1);
 
+       if(smid>64)
+       {
+       for(int sleeptime=0;sleeptime<400;sleeptime++)
+       {
+        __nanosleep(10);
+       }
+       }
+
+
        //judge whther sm id is right
        if((smid < number*SM_NUM)&&(smid >= (number-1)*SM_NUM))
        {
@@ -101,10 +110,7 @@ extern "C" __global__ void fused_nn_conv2d_add_multiply_add_nn_relu_kernel0(int 
        else
        {
        return;
-       for(int sleeptime=0;sleeptime<400;sleeptime++)
-       {
-        __nanosleep(10);
-       }
+
        }
 
     }
