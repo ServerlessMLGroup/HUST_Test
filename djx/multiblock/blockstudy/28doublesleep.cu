e7247110b511a53f99b33a91bd318db1804cee98
hust-yxyhhh <yuyue18@hust.edu.cn>
2023-03-27 16:39:51 +0800

    block study 2

--- djx/multiblock/blockstudy/28doublesleep.cu
+++ djx/multiblock/blockstudy/28doublesleep.cu
@@ -111,7 +111,7 @@ extern "C" __global__ void fused_nn_conv2d_add_multiply_add_nn_relu_kernel0(int 
        {
        for(int sleeptime=0;sleeptime<2000;sleeptime++)
        {
-        __nanosleep(1);
+        __nanosleep(10);
        }
        return;
        }
